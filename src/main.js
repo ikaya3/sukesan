e45f615d28fbfae53c59dc7cff34c10ff12a8f44
Kohta Itagaki <ikaya3@gmail.com>
2018-05-28 00:20:49 +0900

    month/week/day から選択可能とする

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ButtonToolbar } from 'react-bootstrap';
+import { ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
 
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 var request = require('superagent');
 var createReactClass = require('create-react-class');
+
+const WIDTH_TYPE = {
+  DAY  : 0,
+  WEEK : 1,
+  MONTH: 2,
+};
 
 const selectRowProp = {
   mode: 'checkbox',
@@ -41,17 +49,22 @@ var ExpandRow = createReactClass({
 
   componentDidMount: function(){
     console.log("did mount");
-    console.log(this.state.range);
-    console.log(this.getBordersOfDate());
+    this.requestSummary();
+  },
+
+  requestSummary: function(range) {
+    range = range || this.state.range;
+    console.log(range);
+
     request
       .get(__AJAX_SERVER_URI + '/api/mongo_test2')
-      .query({borders_of_date: this.getBordersOfDate().map((v) => v.toJSON()) })
+      .query({borders_of_date: this.getBordersOfDate(range).map((v) => v.toJSON()) })
       .end(function(err,res){
         const pj_detail_copy = this.state.pj_detail;
         console.log("response");
         console.log(res.body);
         res.body.forEach(function(document){ pj_detail_copy[document._id] = []; });
-        this.setState({data: res.body, pj_detail: pj_detail_copy});
+        this.setState({data: res.body, pj_detail: pj_detail_copy, range: range});
       }.bind(this));
   },
 
@@ -76,6 +89,7 @@ var ExpandRow = createReactClass({
   },
 
   onExpand: function(key, isExpand) {
+    console.log(this.state.expanded);
     if(isExpand) {
       console.log(key);
       request
@@ -90,6 +104,11 @@ var ExpandRow = createReactClass({
     }
   },
 
+  onChangeRange: function(value) {
+    console.log(value);
+    this.requestSummary(this.getRange(new Date(), value, this.state.range.count));
+  },
+
   render: function() {
     const range = this.getBordersOfDate().map(
       (v) => (('0' + (v.getMonth() + 1)).slice(-2)) + '/' + (('0' + (v.getDate())).slice(-2)))
@@ -102,6 +121,14 @@ var ExpandRow = createReactClass({
     };
     return (
       <div>
+        <ButtonToolbar>
+          <ToggleButtonGroup type="radio" name="width_type" onChange={this.onChangeRange} defaultValue={WIDTH_TYPE.WEEK}>
+            <ToggleButton value={WIDTH_TYPE.DAY}  >day</ToggleButton>
+            <ToggleButton value={WIDTH_TYPE.WEEK} >week</ToggleButton>
+            <ToggleButton value={WIDTH_TYPE.MONTH}>month</ToggleButton>
+          </ToggleButtonGroup>
+        </ButtonToolbar>
+    
         <BootstrapTable data={this.state.data}
           version="4" hover striped condensed
           options = { options }
@@ -118,25 +145,53 @@ var ExpandRow = createReactClass({
     );
   },
 
-  getInitialRange: function() {
-    const INITIAL_WIDTH = 7;
-    const INITIAL_COUNT = 8;
+  getRange: function(date_target, width_type, count) {
+    var begin_of_date = new Date(date_target.setHours(9,0,0,0));        // TODO : ちょっとダサい
+    switch(width_type){
+      case WIDTH_TYPE.DAY:
+        begin_of_date.setDate(begin_of_date.getDate() - (count-1));
+        break;
+      case WIDTH_TYPE.WEEK:
+        begin_of_date.setDate(begin_of_date.getDate() - begin_of_date.getDay() - (7 * (count-1)));
+        break;
+      case WIDTH_TYPE.MONTH:
+      default:
+        begin_of_date.setDate(1);
+        begin_of_date.setMonth(begin_of_date.getMonth() - (count-1));
+        break;
+    }
 
-    var begin_of_date = new Date(new Date().setHours(9,0,0,0));	// TODO : ちょっとダサい
-    begin_of_date.setDate(begin_of_date.getDate() - begin_of_date.getDay() - (INITIAL_WIDTH * (INITIAL_COUNT-1)));
     console.log(begin_of_date);
 
     return {
+      width_type: width_type,
       begin_of_date: begin_of_date,
-      width: INITIAL_WIDTH,
-      count: INITIAL_COUNT,
+      count: count,
     };
   },
 
-  getBordersOfDate: function() {
-    return Array.apply(null, {length: this.state.range.count + 1}).map((v, i) => {
-      var dt = new Date(this.state.range.begin_of_date);
-      dt.setDate(dt.getDate() + (i * this.state.range.width));
+  getInitialRange: function() {
+    const INITIAL_COUNT = 8;
+    return this.getRange(new Date(), WIDTH_TYPE.WEEK, INITIAL_COUNT);
+  },
+
+  getBordersOfDate: function(range) {
+    range = range || this.state.range;
+    return Array.apply(null, {length: range.count + 1}).map((v, i) => {
+      var dt = new Date(range.begin_of_date);
+      switch(range.width_type){
+        case WIDTH_TYPE.DAY:
+          dt.setDate(dt.getDate() + i);
+          break;
+        case WIDTH_TYPE.WEEK:
+          dt.setDate(dt.getDate() + (i * 7));
+          break;
+        case WIDTH_TYPE.MONTH:
+        default:
+          dt.setDate(1);
+          dt.setMonth(dt.getMonth() + i);
+          break;
+      }
       return dt;
     });
   },
@@ -146,6 +201,7 @@ var ExpandRow = createReactClass({
 ReactDOM.render(
   <div>
     <h1>sukesan</h1>
+
     <ExpandRow/>
 
   {/*
